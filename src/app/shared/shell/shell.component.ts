c7470db0010053b1858b154dcc0a03be268ac8a6
Felix Loehring <loehfelix@gmail.com>
2020-04-03 15:09:33 +0200

    add app shell

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map, shareReplay } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent implements OnInit {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      // adding additional processing to the observed value.
+      // In this case we want to reduce the result to just a boolean that tells us,
+      // whether the current breakpoint matches the specified one.
+
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+  }
+
+}
