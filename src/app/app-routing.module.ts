8f5b9e953b70c629df9ab0f0ca06502f524cca91
Felix Loehring <loehfelix@gmail.com>
2020-04-03 15:50:24 +0200

    add routing for the homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-
-const routes: Routes = [];
+// routes array contains different paths, that are routed to different components
+// -> when the user navigates to a path or certain url, the router will load the component that is registered 
+// under that path
+const routes: Routes = [
+  // the path "myUrl/ + path" will load the HomePageComponent
+  { path: '', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
