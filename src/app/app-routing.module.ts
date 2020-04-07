7dd56664d26c130fb07c7e0047bf9adda740c2e7
Felix Loehring <loehfelix@gmail.com>
2020-04-07 19:08:49 +0200

    add basic auth-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {AuthGuard} from './user/auth.guard';
 
 // routes array contains different paths, that are routed to different components
-// -> when the user navigates to a path or certain url, the router will load the component that is registered 
+// -> when the user navigates to a path or certain url, the router will load the component that is registered
 // under that path
 const routes: Routes = [
   // the path "myUrl/ + path" will load the HomePageComponent
-  { path: '', component: HomePageComponent },
+  {path: '', component: HomePageComponent, canActivate: [AuthGuard]},
   // defining the 'login' path. When accessing that path, loadChildren will lazily load the login component
-  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
+  {path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
