5b442ef053c447256ddfc62a14a64161bae3bb2a
Felix Loehring <loehfelix@gmail.com>
2020-04-08 13:42:10 +0200

    add kanban-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HomePageComponent} from './home-page/home-page.component';
-import {AuthGuard} from './user/auth.guard';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AuthGuard } from './user/auth.guard';
 
 // routes array contains different paths, that are routed to different components
 // -> when the user navigates to a path or certain url, the router will load the component that is registered
 // under that path
 const routes: Routes = [
   // the path "myUrl/ + path" will load the HomePageComponent
-  {path: '', component: HomePageComponent, canActivate: [AuthGuard]},
+  { path: '', component: HomePageComponent },
   // defining the 'login' path. When accessing that path, loadChildren will lazily load the login component
-  {path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
+  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule), canActivate: [AuthGuard] },
 ];
 
 @NgModule({
