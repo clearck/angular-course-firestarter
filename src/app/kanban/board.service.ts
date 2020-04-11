9ae908413a629603891396bd138dc265594164a1
Felix Loehring <loehfelix@gmail.com>
2020-04-11 16:30:11 +0200

    add drag and drop for boards and tasks

--- src/app/kanban/board.service.ts
+++ src/app/kanban/board.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import { switchMap, map } from 'rxjs/operators';
 import { Board, Task } from './board.model';
-import * as firebase from 'firebase';
-import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
-
   constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) { }
 
   /**
@@ -17,7 +16,6 @@ export class BoardService {
    */
   async createBoard(data: Board) {
     const user = await this.afAuth.auth.currentUser;
-    // db.collection('boards') referrs to the collection we created in firebase with the name store
     return this.db.collection('boards').add({
       ...data,
       uid: user.uid,
@@ -25,9 +23,9 @@ export class BoardService {
     });
   }
 
-  /*
-  * Delete board
-  */
+  /**
+   * Delete board
+   */
   deleteBoard(boardId: string) {
     return this.db
       .collection('boards')
@@ -46,20 +44,19 @@ export class BoardService {
   }
 
   /**
-   * Remove a specific task from the board
+   * Remove a specifc task from the board
    */
   removeTask(boardId: string, task: Task) {
     return this.db
       .collection('boards')
       .doc(boardId)
       .update({
-        // remove item from array
         tasks: firebase.firestore.FieldValue.arrayRemove(task)
       });
   }
 
   /**
-   * Get all boards owned by the current user
+   * Get all boards owned by current user
    */
   getUserBoards() {
     return this.afAuth.authState.pipe(
@@ -67,7 +64,8 @@ export class BoardService {
         if (user) {
           return this.db
             .collection<Board>('boards', ref =>
-              ref.where('uid', '==', user.uid).orderBy('priority'))
+              ref.where('uid', '==', user.uid).orderBy('priority')
+            )
             .valueChanges({ idField: 'id' });
         } else {
           return [];
@@ -77,12 +75,13 @@ export class BoardService {
   }
 
   /**
-   * run a batch write to change the priority of each board for sorting
+   * Run a batch write to change the priority of each board for sorting
    */
   sortBoards(boards: Board[]) {
     const db = firebase.firestore();
     const batch = db.batch();
     const refs = boards.map(b => db.collection('boards').doc(b.id));
     refs.forEach((ref, idx) => batch.update(ref, { priority: idx }));
+    batch.commit();
   }
 }
